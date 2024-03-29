a6407f44b9a373a4a5470ad3c01b428986ad09ae
CaioSouzaDevv <caioss20.cs@gmail.com>
2024-03-29 08:39:27 -0300

    [FIX]Teste form

--- php/processor-data.php
+++ php/processor-data.php
@@ -1,34 +1,33 @@
 <?php
-    // Recolher os dados do formulário
-    $nome = isset($_POST['Nome']) ? $_POST['Nome'] : "Não Informado";
-    $email = isset($_POST['Email']) ? $_POST['Email'] : "Não Informado";
-    $telefone = isset($_POST['Telefone']) ? $_POST['Telefone'] : "Não Informado";
-    $site = isset($_POST['Site']) ? $_POST['Site'] : "Não Informado";
-    $mensagem = isset($_POST['Mensagem']) ? $_POST['Mensagem'] : "Não Informado";
+  //Variáveis
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $email = $_POST['telefone'];
+  $mensagem = $_POST['mensagem'];
+  $data_envio = date('d/m/Y');
+  $hora_envio = date('H:i:s');
 
-    // Montar o corpo do email
-    $to = 'caioss20.cs@gmail.com';
-
-    $corpo = "
+  //Compo E-mail
+  $arquivo = "
     <html>
-    <p><b>Nome: </b>$nome</p>
-    <p><b>Nome: </b>$email</p>
-    <p><b>Nome: </b>$telefone</p>
-    <p><b>Nome: </b>$site</p>
-    <p><b>Nome: </b>$mensagem</p>"
-
-    $assunto = "Contato pelo Site";
+      <p><b>Nome: </b>$nome</p>
+      <p><b>E-mail: </b>$email</p>
+      <p><b>Mensagem: </b>$mensagem</p>
+      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
+    </html>
+  ";
   
+  //Emails para quem será enviado o formulário
+  $destino = "edipojoseoliveira@gmail.com";
+  $assunto = "Contato pelo Site";
 
-    // Enviar o email
-    $enviado = mail($to, $assunto, $corpo);
+  //Este sempre deverá existir para garantir a exibição correta dos caracteres
+  $headers  = "MIME-Version: 1.0\n";
+  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+  $headers .= "From: $nome <$email>";
 
-    if ($enviado) {
-        // Redirecionar de volta para a página do formulário
-        header('Location: index.html');
-        exit;
-    } else {
-        // Se houver um erro ao enviar o email, exibir uma mensagem de erro
-        echo "Erro ao enviar o formulário. Por favor, tente novamente mais tarde.";
-    }
+  //Enviar
+  mail($destino, $assunto, $arquivo, $headers);
+  
+  echo "<meta http-equiv='refresh' content='10;URL=../index.html'>";
 ?>
