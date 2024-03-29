f81564d389866ea5bc1c51bb4016d1f63a6c2d7c
CaioSouzaDevv <caioss20.cs@gmail.com>
2024-03-29 08:28:07 -0300

    [FIX]Ajustando Form

---
+++ processor-data.php
@@ -0,0 +1,36 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recolher os dados do formulário
+    $nome = isset($_POST['Nome']) ? $_POST['Nome'] : "Não Informado";
+    $email = isset($_POST['Email']) ? $_POST['Email'] : "Não Informado";
+    $telefone = isset($_POST['Telefone']) ? $_POST['Telefone'] : "Não Informado";
+    $site = isset($_POST['Site']) ? $_POST['Site'] : "Não Informado";
+    $mensagem = isset($_POST['Mensagem']) ? $_POST['Mensagem'] : "Não Informado";
+
+    // Montar o corpo do email
+    $to = 'caioss20.cs@gmail.com';
+
+    $corpo = "
+    <html>
+    <p><b>Nome: </b>$nome</p>
+    <p><b>Nome: </b>$email</p>
+    <p><b>Nome: </b>$telefone</p>
+    <p><b>Nome: </b>$site</p>
+    <p><b>Nome: </b>$mensagem</p>"
+
+    $assunto = "Contato pelo Site";
+  
+
+    // Enviar o email
+    $enviado = mail($to, $assunto, $corpo);
+
+    if ($enviado) {
+        // Redirecionar de volta para a página do formulário
+        header('Location: index.html');
+        exit;
+    } else {
+        // Se houver um erro ao enviar o email, exibir uma mensagem de erro
+        echo "Erro ao enviar o formulário. Por favor, tente novamente mais tarde.";
+    }
+}
+?>
